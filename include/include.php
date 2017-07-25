b0969a70182ba846f81cb6304c82b20430c33073
saiwolf <saiwolf@swmnu.net>
2017-07-25 15:02:21 +0000

    Commit

--- include/include.php
+++ include/include.php
@@ -18,7 +18,7 @@
  */
 
 namespace TicketApp;
- 
+
 Class GenericHelper
 {
     /**
@@ -34,5 +34,61 @@ Class GenericHelper
          $pageTitle = pathinfo($safeFile, PATHINFO_FILENAME);
          return ucfirst($pageTitle);
      }
+}
+
+Class DBHelper
+{
+    /**
+     * Class constructor for DBHelper.
+     * Defines class-wide variables for
+     * database connection and maintenance.
+     * 
+     * We declare TicketApp\Config here via 'new'
+     * to keep our DB values out of source control
+     * for security reasons.
+     */
+    public function __construct() {
+        
+        $this->dbuser = 'TicketApp';
+        $this->dbpass = 'vZvFqpLxc5dEq749';
+        $this->dbhost = 'localhost';
+        $this->dbname = 'TicketApp';
+    }
+    
+    /**
+     * Class destructor for DBHelper
+     * Disconnects any open DB connection.
+     */
+    public function __destruct() {
+        $this->disconnect();
+    }
+    
+    /**
+     * Function that attempts to establish a connection
+     * to MySQL via PDO. Uses values established in
+     * parent constructor as connection values.
+     * 
+     * @return class $dbConn Returns a new PDO class object on success, false on failure.
+     */
+    public function connect() {
+        try {
+            $this->dbConn = new \PDO("mysql:host=" . $this->dbhost . ";database=" . $this->dbname . "", $this->dbuser, $this->dbpass);
+            $this->dbConn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            return $this->dbConn;
+        } 
+        catch (\PDOException $ex) {
+            $errorMessage =  "PDO caught an error! MySQL said: " . $ex->GetMessage() . "<br />";
+            return $errorMessage;
+        }
+    }
+    
+    /**
+     * Function that sets class object
+     * dbConn to null, therefore closing
+     * any open SQL PDO connections.
+     */
+    public function disconnect() {
+        $this->dbConn = null;
+    }
 }
 ?>
--- public/index.php
+++ public/index.php
@@ -1,14 +1,34 @@
-<?php require(__DIR__ . '/../include/layouts/head.inc'); ?>
+<?php
+  /**
+   * Require the head layout
+   * 
+   */
+  require(__DIR__ . '/../include/layouts/head.inc');
+  
+  /**
+   * Invoking DBHelper Class. We need it for SQL operations.
+   * 
+   */
+  use TicketApp\DBHelper;
+  $db = new DBHelper;
+  $dbConn = $db->connect();
+  echo var_dump($dbConn);
+  
+  $stmt = $dbConn->query("SELECT * FROM tickets WHERE EXISTS (SELECT name FROM people)");
+  echo $stmt;
+  
+?>
 <div class="row">
     <p></p>
-    <h1>Whelp</h1>
+    <h1>Tickets</h1>
+<?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default panel-table">
               <div class="panel-heading">
                 <div class="row">
                   <div class="col col-xs-12">
-                    <h3 class="panel-title">Random Assigned User #1</h3>
+                    <h3 class="panel-title"><?php echo $row['AssignedTo']; ?></h3>
                   </div>
                 </div>
               </div>
@@ -33,147 +53,23 @@
                             <td align="center">
                               <a class="btn btn-default"><em class="fa fa-pencil fa-fw"></em></a>
                             </td>
-                            <td class="hidden-xs">23</td>
-                            <td>2/13/2017</td>
-                            <td>John Doe</td>
-                            <td>Test desc</td>
-                            <td>Random Dept.</td>
-                            <td>Random Assigned Person #1</td>
-                            <td>50</td>
-                            <td></td>
-                            <td><a class="btn btn-danger"><em class="fa fa-trash fa-fw"></em></a></td>
-                          </tr>
-                          <tr>
-                            <td align="center">
-                              <a class="btn btn-default"><em class="fa fa-pencil fa-fw"></em></a>
-                            </td>
-                            <td class="hidden-xs">28</td>
-                            <td>2/13/2017</td>
-                            <td>John Doe</td>
-                            <td>Test desc</td>
-                            <td>Random Dept.</td>
-                            <td>Random Assigned Person #1</td>
-                            <td>50</td>
-                            <td></td>
-                            <td><a class="btn btn-danger"><em class="fa fa-trash fa-fw"></em></a></td>
-                          </tr>
-                          <tr>
-                            <td align="center">
-                              <a class="btn btn-default"><em class="fa fa-pencil fa-fw"></em></a>
-                            </td>
-                            <td class="hidden-xs">236</td>
-                            <td>2/13/2017</td>
-                            <td>John Doe</td>
-                            <td>Test desc</td>
-                            <td>Random Dept.</td>
-                            <td>Random Assigned Person #1</td>
-                            <td>50</td>
-                            <td></td>
+                            <td class="hidden-xs"><?php echo $row['id']; ?></td>
+                            <td><?php echo $row['SubmittedOn']; ?></td>
+                            <td><?php echo $row['RequestorName']; ?></td>
+                            <td><?php echo $row['Description']; ?></td>
+                            <td><?php echo $row['Department']; ?></td>
+                            <td><?php echo $row['AssignedTo']; ?></td>
+                            <td>TODO - Days open</td>
+                            <td><?php echo $row['CompletedOn']; ?></td>
                             <td><a class="btn btn-danger"><em class="fa fa-trash fa-fw"></em></a></td>
                           </tr>
                         </tbody>
                 </table>
-            
               </div>
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default panel-table">
-              <div class="panel-heading">
-                <div class="row">
-                  <div class="col col-xs-12">
-                    <h3 class="panel-title">Random Assigned User #2</h3>
-                  </div>
-                </div>
-              </div>
-              <div class="panel-body">
-                <table class="table table-striped table-bordered table-list">
-                  <thead>
-                    <tr>
-                        <th><em>Edit</em></th>
-                        <th class="hidden-xs">Ticket #</th>
-                        <th>Submitted On</th>
-                        <th>Name</th>
-                        <th>Description</th>
-                        <th>Department</th>
-                        <th>Assigned To</th>
-                        <th>Days Open</th>
-                        <th>Date Completed</th>
-                        <th><em>Delete</em></th>
-                    </tr> 
-                  </thead>
-                  <tbody>
-                          <tr>
-                            <td align="center">
-                              <a class="btn btn-default"><em class="fa fa-pencil fa-fw"></em></a>
-                            </td>
-                            <td class="hidden-xs">23</td>
-                            <td>2/13/2017</td>
-                            <td>John Doe</td>
-                            <td>Test desc</td>
-                            <td>Random Dept.</td>
-                            <td>Random Assigned Person #1</td>
-                            <td>50</td>
-                            <td></td>
-                            <td><a class="btn btn-danger"><em class="fa fa-trash fa-fw"></em></a></td>
-                          </tr>
-                        </tbody>
-                </table>
-            
-              </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default panel-table">
-              <div class="panel-heading">
-                <div class="row">
-                  <div class="col col-xs-12">
-                    <h3 class="panel-title">Random Assigned User #3</h3>
-                  </div>
-                </div>
-              </div>
-              <div class="panel-body">
-                <table class="table table-striped table-bordered table-list">
-                  <thead>
-                    <tr>
-                        <th><em>Edit</em></th>
-                        <th class="hidden-xs">Ticket #</th>
-                        <th>Submitted On</th>
-                        <th>Name</th>
-                        <th>Description</th>
-                        <th>Department</th>
-                        <th>Assigned To</th>
-                        <th>Days Open</th>
-                        <th>Date Completed</th>
-                        <th><em>Delete</em></th>
-                    </tr> 
-                  </thead>
-                  <tbody>
-                          <tr>
-                            <td align="center">
-                              <a class="btn btn-default"><em class="fa fa-pencil fa-fw"></em></a>
-                            </td>
-                            <td class="hidden-xs">23</td>
-                            <td>2/13/2017</td>
-                            <td>John Doe</td>
-                            <td>Test desc</td>
-                            <td>Random Dept.</td>
-                            <td>Random Assigned Person #1</td>
-                            <td>50</td>
-                            <td></td>
-                            <td><a class="btn btn-danger"><em class="fa fa-trash fa-fw"></em></a></td>
-                          </tr>
-                        </tbody>
-                </table>
-            
-              </div>
-            </div>
-        </div>
-    </div>
+<?php } // End DB While Loop ?>
 </div>
 <?php require(__DIR__ . '/../include/layouts/footer.inc'); ?>
 
