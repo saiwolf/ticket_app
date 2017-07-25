63c334d265a50be21ba6bceb00aacdef672e2712
saiwolf <saiwolf@swmnu.net>
2017-07-25 16:22:41 +0000

    Index 98 percent done

--- public/index.php
+++ public/index.php
@@ -9,35 +9,43 @@
 
 use TicketApp\DBHelper;
 
-$db = new DBHelper;
+$db1 = new DBHelper;
+$db2 = new DBHelper;
 
-$db->query("SELECT * FROM tickets WHERE EXISTS (SELECT name FROM people)");
-
-$dataset = $db->resultSet();
-
+$db1->query("SELECT name FROM people");
+$nameset = $db1->resultSet();
 
 
 ?>
 <div class="row">
-    <p></p>
-    <h1>Tickets</h1>
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default panel-table">
+  <div class="col-md-10 col-md-offset-1">
+    <h3>Tickets!</h3>
+  </div>
+</div>
+
+<?php
+  foreach ($nameset as $data) {
+    $db2->query("SELECT * FROM tickets WHERE AssignedTo = :name");
+    $db2->bind('name', $data['name'], PDO::PARAM_STR);
+    $dataset = $db2->resultSet();
+?>
+<div class="row">
+  <div class="col-md-10 col-md-offset-1">
+    <div class="panel panel-default panel-table">
+
               <div class="panel-heading">
                 <div class="row">
                   <div class="col col-xs-12">
-                    <h3 class="panel-title"></h3>
+                    <h3 class="panel-title"><?php echo $data['name']; ?></h3>
                   </div>
                 </div>
               </div>
-              <?php foreach ($dataset as $row) { ?>
+              
               <div class="panel-body">
                 <table class="table table-striped table-bordered table-list">
                   <thead>
                     <tr>
                         <th><em>Edit</em></th>
-                        <th class="hidden-xs">Ticket #</th>
                         <th>Ticket #</th>
                         <th>Submitted On</th>
                         <th>Name</th>
@@ -46,15 +54,15 @@ $dataset = $db->resultSet();
                         <th>Assigned To</th>
                         <th>Days Open</th>
                         <th>Date Completed</th>
-                        <th><em>Delete</em></th>
                     </tr> 
                   </thead>
                   <tbody>
+                    <?php foreach ($dataset as $row) { ?>
                           <tr>
                             <td align="center">
-                              <a class="btn btn-default"><em class="fa fa-pencil fa-fw"></em></a>
+                              <a class="btn btn-default"><em class="fa fa-pencil"></em></a>
+                              <a class="btn btn-danger"><em class="fa fa-trash"></em>
                             </td>
-                            <td class="hidden-xs"><?php echo $row['id']; ?></td>
                             <td><?php echo $row['id']; ?></td>
                             <td><?php echo $row['SubmittedOn']; ?></td>
                             <td><?php echo $row['RequesterName']; ?></td>
@@ -63,16 +71,14 @@ $dataset = $db->resultSet();
                             <td><?php echo $row['AssignedTo']; ?></td>
                             <td>50</td>
                             <td><?php echo $row['CompletedOn']; ?></td>
-                            <td></td>
-
-                            <td><a class="btn btn-danger"><em class="fa fa-trash fa-fw"></em></a></td>
                           </tr>
-                        </tbody>
+                      <?php } ?>
+                    </tbody>
                 </table>
               </div>
-              <?php } ?>
             </div>
         </div>
     </div>
+<?php } ?>
 </div>
 <?php require(__DIR__ . '/../include/layouts/footer.inc'); ?>
